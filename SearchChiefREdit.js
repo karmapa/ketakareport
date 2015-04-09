fdf9497e1f596e233381b87e5694f77a98f2daf5
circleuniv <circleuniv@gmail.com>
2015-04-09 19:34:59 +0800

    Glob all file & Get Unique item
    Glob 問題待處理

--- SearchChiefREdit.js
+++ SearchChiefREdit.js
@@ -1,30 +1,64 @@
-var fs=require("fs");
-var file=JSON.parse(fs.readFileSync("./0304-001.json","utf8"));
+var glob=require("glob");
 
-var Person_obj={}; //chief proof reader 名單
-var DataGroup= []; //[chief proff reader, 修改字起始,長度,頁數,修改字]
 
-file.rows.map(function(a){
-  if(a.doc.payload.type =="revision")
-  {
-       if(!(a.doc.payload.author in Person_obj))  	   
-  	   	   Person_obj[a.doc.payload.author]=a.doc.payload.author;
-  }
+function CheckBound(filename){
 
-});
+	var fs=require("fs");  
+	var file=JSON.parse(fs.readFileSync(filename,"utf8"));
 
-var arr=Object.keys(Person_obj).map(function(kwd)
-{	
-	 file.rows.map(function(a){
 
-		if(a.doc.payload.type =="suggest" && kwd==a.doc.payload.author)
-		{
-		  DataGroup.push([a.doc.payload.author,
-			a.doc.start,a.doc.len,a.doc.pageid,a.doc.len,a.doc.payload.text]);	
-          
-		}
-	});        
-});
+	var Person_obj={}; //chief proof reader 名單
+	var DataGroup= []; //[chief proff reader, 修改字起始,長度,頁數,修改字]
 
+	file.rows.map(function(a){
+	  if(a.doc.payload.type =="revision")
+	  {
+	       if(!(a.doc.payload.author in Person_obj))  	   
+	  	   	   Person_obj[a.doc.payload.author]=a.doc.payload.author;
+	  }
+
+	});
+
+	var arr=Object.keys(Person_obj).map(function(kwd)
+	{	
+		 file.rows.map(function(a){
+
+			if(a.doc.payload.type =="suggest" && kwd==a.doc.payload.author)
+			{
+				var Arrpush=[a.doc.payload.author,
+				a.doc.start,a.doc.len,a.doc.pageid,a.doc.len,a.doc.payload.text];
+                
+                DataGroup.push(Arrpush);   
+			}
+		});        
+		
+	});
+    
+DataGroup=DataGroup.ToUnique();
 console.log(DataGroup);
-fs.writeFileSync("SerchChiefWordsResults.json",JSON.stringify(DataGroup,""," "),"utf8");
+
+}
+
+Array.prototype.ToUnique = function()
+{
+	var n = {},r=[];
+	for(var i = 0; i < this.length; i++) 
+	{
+		if (!n[this[i]]) 
+		{
+			n[this[i]] = true; 
+			r.push(this[i]); 
+		}
+	}
+	return r;
+}
+
+//fs.writeFileSync("SerchChiefWordsResults.json",JSON.stringify(DataGroup,""," "),"utf8");
+
+//CheckBound("0304-001.json");
+glob("./*/*/*.json",function(err,files){
+        files.map(CheckBound);
+ 		//console.log(files);     
+    
+});
+
