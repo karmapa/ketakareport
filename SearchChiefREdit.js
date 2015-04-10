ed04e872edd4f1540fd1e8badfb8501b94628d72
circleuniv <circleuniv@gmail.com>
2015-04-10 15:16:37 +0800

    程式碼整理

--- SearchChiefREdit.js
+++ SearchChiefREdit.js
@@ -2,20 +2,19 @@ var glob=require("glob");
 var fs=require("fs");
 var unique=require("./unique");
 
-var dataGroup= []; //[chief proff reader, 修改字起始,長度,頁數,修改字]
+var dataGroup= []; //[chief proof reader, 修改字起始,長度,頁數,修改字]
 
 function checkBound(filename){
-	//console.log(filename);
+
 	var content=fs.readFileSync(filename,"utf8");
 	var file=null;
 	try {
 		file=JSON.parse(content); 
 	} catch(e) {
-		console.log(e)
-		throw "error handling file"+filename
+		console.log(e);
+		throw "error handling file"+filename;
 	}
 	var Person_obj={}; //chief proof reader 名單
-	//console.log(filename);
 
 	file.rows.map(function(a){
 	  if(a.doc.payload.type =="revision")
@@ -46,28 +45,8 @@ function checkBound(filename){
 
 
 glob("./*/**/**/*.json",function(err,files){
-	//console.log(files);
     files.map(checkBound);    
     unique(dataGroup);
     console.log(dataGroup);
     fs.writeFileSync("Result_SerchChiefWords.json",JSON.stringify(dataGroup,""," "),"utf8");
 });
-
-// var glob=require("glob");
-
-// var fs=require("fs"); 
-
-// function checknow(filename){
-	     
-//   var rt=fs.readFileSync(filename,"utf8");
-//   var c=rt.match(/\"key\":\"jiangkangyurlj0304_001_lakdor_3_240\",\"value\":{\"rev\":\"23-8804/g);  
-//    if(c!=null)
-//   console.log(filename);
-// }
-
-// glob("./*/**/**/*.json",function(err,files){
-// //	console.log(files);
-//      files.map(checknow);    
-//     // unique(dataGroup)
-//     // fs.writeFileSync("Result_SerchChiefWords.json",JSON.stringify(dataGroup,""," "),"utf8");
-// });
